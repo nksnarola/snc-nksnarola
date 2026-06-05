b35bd810b8d1ba24d0ffb795617280cd20f52a6b
Nimesh Savaliya <nks@narola.email>
2026-06-05 11:39:11 +0530

    - button style changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,9 @@ export const Button: FunctionComponent<PropsWithChildren<ButtonProps>> = ({
   return (
     <button
       type="button"
-      className={classNames("px-2 py-1 border border-black")}
+      className={classNames(
+        "px-3 py-1.5 rounded-lg border border-gray-300 text-sm font-medium text-gray-700 hover:bg-gray-50 transition-all",
+      )}
     >
       {children}
     </button>
